fa87794de2833acae33360aabfca62cac6dace03
frnmorfnrgg <zenitsoblack444@gmail.com>
2026-04-18 00:54:19 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const cacheName = 'finance-pro-v2';
-const assets = ['./', './index.html', './manifest.json', './icon.png'];
+const cacheName = 'finance-pro-v1';
+const assets = ['./', './index.html'];
 
 self.addEventListener('install', e => {
   e.waitUntil(
@@ -11,16 +11,4 @@ self.addEventListener('fetch', e => {
   e.respondWith(
     caches.match(e.request).then(res => res || fetch(e.request))
   );
-});
-
-// ESCUTA MENSAGENS PARA DISPARAR NOTIFICAÇÃO
-self.addEventListener('message', event => {
-    if (event.data && event.data.type === 'NOTIFICAR_VENCIMENTO') {
-        self.registration.showNotification('Finance PRO: Lucro Disponível! 🚀', {
-            body: `O seu lucro de ${event.data.valor} já pode ser coletado!`,
-            icon: './icon.png',
-            vibrate: [200, 100, 200],
-            tag: 'vencimento-lucro'
-        });
-    }
 });
